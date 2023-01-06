4092414fad9a25dfcb3dfda31db8268e5139c2bc
i.shirinov <shirinovilyos6965@gmail.com>
2023-01-06 16:22:40 +0500

    1

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Generics {
 
     //TODO: Refactor Method-1
+    public List Generics(String name){
+        List<String> list = boxingMethod(name);
+        return list;
+    }
     public List boxingMethod(String name) {
         List firstList = new ArrayList<>();
         firstList.add(name);
@@ -15,11 +19,17 @@ public class Generics {
     }
 
     //TODO: Refactor Method-2
+    public Object Generics(Object data){
+        return genericMethod(data);
+    }
     public Object genericMethod(Object data) {
         return data;
     }
 
     //TODO: Refactor Method-3
+    public void Generics(List consumer, List producer){
+        cloneMethod(consumer, producer);
+    }
     public void cloneMethod(List consumer, List producer) {
         consumer.addAll(producer);
     }
